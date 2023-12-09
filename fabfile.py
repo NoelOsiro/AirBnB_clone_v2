e259edc956e9a256e646b06015f7c37c5c6f4ac2
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-12-09 04:09:38 +0300

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,5 @@ env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 
 
 def hello():
-target = run('ls -t ./AirBnB_Clone_V2/versions/')
-print(target.split())
+  target = run('ls -t ./AirBnB_Clone_V2/versions/')
+  print(target.split())
