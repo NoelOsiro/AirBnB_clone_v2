3d98c26e7ba3c7a5f257cbd4bdfeb399ae7d0540
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-01-26 02:01:36 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-    Sript that starts a Flask web application
-"""
+"""Flask framework"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def handle_teardown(self):
-    """
-        method to handle teardown
-    """
-    storage.close()
+def teardown_db(exception):
+    """ teardown db"""
+    if storage is not None:
+        storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def state_list():
-    """
-        method to render states
-    """
-    states = storage.all('State').values()
-    return render_template("7-states_list.html", states=states)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ list of state ids"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
+
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
